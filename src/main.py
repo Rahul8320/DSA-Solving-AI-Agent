cc6d1e2465306a64c9bf0c91074fc2b10b58ab3b
Rahul Dey <rahuldey832000@gmail.com>
2025-07-06 19:46:32 +0530

    Added code executor agent

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from dsa-solving-agent!")
+import asyncio
+
+from agents.code_executor import CodeExecutor
+
+
+async def main():
+    content: str = '''Here is some code
+```python
+def add(a:int, b:int)->int:
+    return a+b
+    
+print('Hello world')
+result = add(5,3)
+print(f"Sum of 5 and 3 is: {result}")
+```
+'''
+    await CodeExecutor.run(content=content)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
